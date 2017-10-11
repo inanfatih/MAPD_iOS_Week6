56e3730afd78319ca0b78773cd309cced0c52799
Student <student@setass-mac-mini-10.local>
2017-10-11 19:24:26 -0400

    first commit

---
+++ MADP-Lesson6/SingleComponentPickerViewController.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+class SingleComponentPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
